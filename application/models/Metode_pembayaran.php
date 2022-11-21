14f10725f9999a467839357cdeb2e6760b5d303d
andredwisri <andredwinurlita@gmail.com>
2022-11-21 19:38:22 +0700

    push model metode pembayaran

---
+++ application/models/Metode_pembayaran.php
@@ -0,0 +1,33 @@
+<?php 
+class Metode_pembayaran extends CI_Model {
+
+    public $Jenis_pembayaran;
+    public $No_account;
+    public $Logo;
+    
+    public function read_all()
+    {
+        return $query = $this->db->get('Metode_pembayaran')->result();
+            
+    }
+    public function create($data)
+    {
+        $this->Jenis_pembayaran = $data['Jenis_pembayaran'];
+        $this->No_account = $data['No_account'];
+        $this->Logo = $data['Logo'];
+        $this->db->insert('Metode_pembayaran', $this);
+    }
+    
+    public function update($data)
+    {
+    
+        $this->Jenis_pembayaran = $data['Jenis_pembayaran'];
+        $this->No_account = $data['No_account'];
+        $this->Logo = $data['Logo'];
+        $this->db->update('Metode_pembayaran', $this, ['Metode_pembayaran' = $data['Metode_pembayaran']]);
+    }
+
+    public function delete($Metode_pembayaran)
+    {
+        $this->db->delete('Metode_pembayaran', ['Metode_pembayaran' = $Metode_pembayaran]);
+    }     
